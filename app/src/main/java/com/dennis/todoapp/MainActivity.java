a57f83384f60ee58e1844cbcdd52b08a135492f8
Dennis-Kioko <kiokodennis6990@gmail.com>
2021-07-02 19:43:59 +0300

    First git commit

---
+++ app/src/main/java/com/dennis/todoapp/MainActivity.java
@@ -0,0 +1,32 @@
+ package com.dennis.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mainButton = findViewById(R.id.btn_main);
+
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ToDoActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/dennis/todoapp/ToDoActivity.java
@@ -0,0 +1,56 @@
+package com.dennis.todoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.dennis.todoapp.databinding.ActivityToDoBinding;
+
+public class ToDoActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityToDoBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        CardView singleCard = findViewById(R.id.card_single);
+        singleCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ToDoActivity.this,ToDoDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        setSupportActionBar(toolbar);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}

1d90001b6676f7f5d8cd42065b5963ca3e3dce51
Dennis-Kioko <kiokodennis6990@gmail.com>
2021-07-07 19:45:23 +0300

    Added an activity to create task

--- app/src/main/java/com/dennis/todoapp/ToDoDetailActivity.java
+++ app/src/main/java/com/dennis/todoapp/ToDoDetailActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 import com.dennis.todoapp.databinding.ActivityToDoDetailBinding;
 
@@ -28,6 +29,17 @@ public class ToDoDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
+
+        Button btnComplete = findViewById(R.id.btn_complete);
+
+      btnComplete.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+
+              Snackbar.make(v, "Task added successfully", Snackbar.LENGTH_LONG)
+                      .setAction("Action", null).show();
+          }
+      });
 
         FloatingActionButton fab = binding.fab;
         fab.setOnClickListener(new View.OnClickListener() {

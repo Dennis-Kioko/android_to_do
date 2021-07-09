9ca5697c4d1e228950dcc62609d0ac530e3c3361
Dennis-Kioko <kiokodennis6990@gmail.com>
2021-07-09 19:10:57 +0300

    Attached New to do components to java

--- app/src/main/java/com/dennis/todoapp/NewToDoActivity.java
+++ app/src/main/java/com/dennis/todoapp/NewToDoActivity.java
@@ -2,7 +2,16 @@ package com.dennis.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class NewToDoActivity extends AppCompatActivity {
 
@@ -10,5 +19,42 @@ public class NewToDoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_to_do);
+
+        Button btnCancel = findViewById(R.id.btnCancel);
+
+        Button btnCreateTask = findViewById(R.id.btnCreateTask);
+
+
+        ImageButton btnAddSubtask = findViewById(R.id.btnAddSubtask);
+
+
+        ImageView imgFirstFile = findViewById(R.id.imgFirstFile);
+
+        ImageView imgSecondFile = findViewById(R.id.imgSecondFile);
+
+
+        TextInputEditText editTitle = findViewById(R.id.editTitle);
+
+        TextInputEditText editDetails = findViewById(R.id.editDetails);
+
+        TextInputEditText editSubtasks = findViewById(R.id.editSubtasks);
+
+        btnCreateTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String Title = editTitle.getText().toString();
+                String Details = editDetails.getText().toString();
+
+                Toast.makeText(NewToDoActivity.this,Title, Toast.LENGTH_SHORT).show();
+
+                Snackbar.make(v, Details, Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+
     }
 }
